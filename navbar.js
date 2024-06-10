971482f606bdffa0819429920fd05833a78f2a79
Ondřej Fíla <114986357+ondrejfila6969@users.noreply.github.com>
2024-06-10 14:54:46 +0200

    FINAL2

--- navbar.js
+++
@@ -1,6 +0,0 @@
-const hamburgerNavbar = document.getElementsByClassName("navbar-hamburger")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
-hamburgerNavbar.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-})
